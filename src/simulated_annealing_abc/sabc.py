e57197955f37548537512bedc5abacd3dc0e1437
Stefan Weber <s.weber0688@gmail.com>
2026-02-16 12:32:02 +0100

    chore: replace lambda with def

--- src/simulated_annealing_abc/sabc.py
+++ src/simulated_annealing_abc/sabc.py
@@ -65,7 +65,8 @@ def update_epsilon_single_eps(u_bar: float, v: float) -> np.ndarray:
     if u_bar <= 1e-12:
         return np.array([0.0], dtype=float)
 
-    f = lambda eps: eps**2 + v * eps**1.5 - u_bar**2
+    def f(eps: float, u_bar: float) -> float:
+        return eps**2 + v * eps**1.5 - u_bar**2
 
     sol = root_scalar(f, bracket=(0.0, u_bar))
     if not sol.converged:
