cfcf7df4a635e29669a105d859a6e2828b99cd4e
Stefan Weber <s.weber0688@gmail.com>
2026-02-18 11:00:19 +0100

    chore: add opencode

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
 
+env_name="sabc-dev"
 
-
-# ## install mkdocs env
-micromamba create --file environment.sabc-dev.yml --yes
+# ## install env
+micromamba create --file environment.${env_name:?}.yml --yes
 # ## clean pip cache
-micromamba run -n mkdocs python -m pip cache purge
+micromamba run -n ${env_name} python -m pip cache purge
 # ## clean micromamba cache
 micromamba clean --all --force-pkgs-dirs --yes
+
+# ## install npm packages
+micromamba run -n ${env_name} npm install -g \
+    opencode-ai \
+    markdownlint-cli2 markdownlint-cli2-formatter-pretty markdownlint-cli2-formatter-summarize
+# ## clean npm cache
+micromamba run -n ${env_name} npm cache clean --force
+
+# ## opencode setup (ensure auth.json is symlinked to the correct location)
+if [ -f "${WORKSPACE_FOLDER:?}/.opencode/auth.json" ]; then
+    mkdir -p ~/.local/share/opencode
+    ln -s ${WORKSPACE_FOLDER:?}/.opencode/auth.json ~/.local/share/opencode/auth.json
+else
+    echo "Warning: ${WORKSPACE_FOLDER:?}/.opencode/auth.json not found. Please ensure you have the correct auth.json file for opencode and symlink it to ~/.local/share/opencode/auth.json"
+fi
